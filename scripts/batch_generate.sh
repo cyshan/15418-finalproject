d894d1b1e46b7dcb066dac476fb2537959618a1d
CShan <mngshan@yahoo.com>
2017-11-17 16:56:19 -0500

    Initial commit

---
+++ scripts/batch_generate.sh
@@ -0,0 +1,14 @@
+#/usr/bin/env bash
+# generate jobs in batch
+
+threads=(32) # The number of threads 
+inputs=(16x16.txt) # The name of the input files
+rm -f *.job
+
+for f in ${inputs[@]}
+do
+    for t in ${threads[@]}
+    do
+	../scripts/generate_jobs.sh $f $t
+    done
+done
---
+++ sudoku.cpp
@@ -0,0 +1,249 @@
+/**
+ * Parallel Sudoku solver
+ * Christopher Shan(cshan1), Omar Shafie(oshafie)
+ */
+
+#include <chrono>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <assert.h>
+#include <omp.h>
+#include "mic.h"
+
+#define BUFSIZE 1024
+//number of bits (least significant) used to store the real value of the board cell
+#define VALUEBITS 5
+
+static int _argc;
+static const char **_argv;
+
+/* Starter code function, don't touch */
+const char *get_option_string(const char *option_name,
+            const char *default_value)
+{
+  for (int i = _argc - 2; i >= 0; i -= 2)
+    if (strcmp(_argv[i], option_name) == 0)
+      return _argv[i + 1];
+  return default_value;
+}
+
+/* Starter code function, do not touch */
+int get_option_int(const char *option_name, int default_value)
+{
+  for (int i = _argc - 2; i >= 0; i -= 2)
+    if (strcmp(_argv[i], option_name) == 0)
+      return atoi(_argv[i + 1]);
+  return default_value;
+}
+
+/* Starter code function, do not touch */
+float get_option_float(const char *option_name, float default_value)
+{
+  for (int i = _argc - 2; i >= 0; i -= 2)
+    if (strcmp(_argv[i], option_name) == 0)
+      return (float)atof(_argv[i + 1]);
+  return default_value;
+}
+
+/* Starter code function, do not touch */
+static void show_help(const char *program_path)
+{
+    printf("Usage: %s OPTIONS\n", program_path);
+    printf("\n");
+    printf("OPTIONS:\n");
+    printf("\t-f <input_filename> (required)\n");
+    printf("\t-n <num_of_threads> (required)\n");
+}
+
+
+int maxInt(int a, int b) { return (a > b)? a : b; }
+int minInt(int a, int b) { return (a < b)? a : b; }
+
+void addToBoard(int num, int i, int *board, int boardSize) {
+  /*temporary storage for the bitvector specifying what number choices still open for
+    that spot on the board */
+  int choices;
+  if (num) {
+    //num != 0
+    //fixed number on sudoku board, so only 1 choice for the number
+    choices = 1 << num;
+
+  } else {
+    //num == 0
+    //blank number on sudoku board, so can be any number
+    choices = ((1 << boardSize) - 1) << 1;
+  }
+  board[i] = (choices << VALUEBITS) + num;
+}
+
+void writeFile(FILE *output_file, int *board, int i) {
+  fprintf(output_file, "%d ", board[i] % (1<<VALUEBITS));
+}
+
+void ellimination(int *board, int boardSize, bool &cellChanged) {
+
+}
+
+void loneRanger(int *board, int boardSize, bool &cellChanged) {
+
+}
+
+void twins(int *board, int boardSize, bool &choicesChanged) {
+
+}
+
+void triplets(int *board, int boardSize, bool &choicesChanged) {
+
+}
+
+bool humanistic(int *board, int boardSize) {
+  /* return false if board has no solution
+     otherwise (i.e. if solution found or algorithm makes no more changes) return true */
+
+  //in some step of algorithm, a cell was written to with its final value
+  bool cellChanged = true;
+  //in some step of algorithm, choices were elliminated from some cell
+  bool choicesChanged = true;
+  while (cellChanged || choicesChanged) {
+    if (cellChanged) {
+      cellChanged = false;
+      ellimination(board, boardSize, cellChanged);
+      if (cellChanged) continue;
+      loneRanger(board, boardSize, cellChanged);
+      if (cellChanged) continue;
+    }
+    if (choicesChanged) {
+      choicesChanged = false;
+      twins(board, boardSize, choicesChanged);
+      if (choicesChanged) continue;
+      triplets(board, boardSize, choicesChanged);
+    }
+  }
+  return true;
+}
+
+int main(int argc, const char *argv[])
+{
+  using namespace std::chrono;
+  typedef std::chrono::high_resolution_clock Clock;
+  typedef std::chrono::duration<double> dsec;
+
+  auto init_start = Clock::now();
+  double init_time = 0;
+ 
+  _argc = argc - 1;
+  _argv = argv + 1;
+
+  srand(time(NULL));
+
+  /* You'll want to use these parameters in your algorithm */
+  const char *input_filename = get_option_string("-f", NULL);
+  int num_of_threads = get_option_int("-n", 1);
+
+  int error = 0;
+
+  if (input_filename == NULL) {
+    printf("Error: You need to specify -f.\n");
+    error = 1;
+  }
+
+  if (error) {
+    show_help(argv[0]);
+    return 1;
+  }
+  
+  printf("Number of threads: %d\n", num_of_threads);
+  printf("Input file: %s\n", input_filename);
+
+  FILE *input = fopen(input_filename, "r");
+
+  if (!input) {
+    printf("Unable to open file: %s.\n", input_filename);
+    return -1;
+  }
+ 
+  //size of sudoku board is n^2 x n^2
+  int n;
+
+  fscanf(input, "%d\n", &n);
+
+  int boardSize = n*n;
+
+  int *board = (int *)calloc(boardSize * boardSize, sizeof(int));
+
+  //for parsing sudoku board
+  int num;
+ 
+  for (int row = 0; row < boardSize; row++) {
+    for (int col = 0; col < boardSize; col++) {
+      int i = row * boardSize + col;
+      fscanf(input, "%d", &num);
+      addToBoard(num, i, board, boardSize);
+    }
+    fscanf(input, "\n");
+  }
+
+
+  /* Initialize additional data structures needed in the algorithm 
+   * here if you feel it's needed. */
+
+
+  error = 0;
+
+  init_time += duration_cast<dsec>(Clock::now() - init_start).count();
+  printf("Initialization Time: %lf.\n", init_time);
+  auto compute_start = Clock::now();
+  double compute_time = 0;
+
+
+#ifdef RUN_MIC /* Use RUN_MIC to distinguish between the target of compilation */
+
+  /* This pragma means we want the code in the following block be executed in 
+   * Xeon Phi.
+   */
+#pragma offload target(mic)  \
+  inout(board: length(boardSize * boardSize) INOUT) 
+#endif
+  {
+
+    //Humanistic algorithm
+    if (!humanistic(board, boardSize)){
+      //no solution exists
+    }
+
+  }
+
+  compute_time += duration_cast<dsec>(Clock::now() - compute_start).count();
+  printf("Computation Time: %lf.\n", compute_time);
+  
+  /* OUTPUT YOUR RESULTS TO FILES HERE */
+  char input_filename_cpy[BUFSIZE];
+  strcpy(input_filename_cpy, input_filename);
+  char *filename = basename(input_filename_cpy);
+  filename[strlen(filename) - 4] = '\0';
+  char output_filename[BUFSIZE];
+
+
+  sprintf(output_filename, "file_outputs/output_%s_%d.txt", filename, num_of_threads);
+  FILE *output_file = fopen(output_filename, "w");
+  if (!output_file) {
+    printf("Error: couldn't output file");
+    return -1;
+  }
+
+  fprintf(output_file, "%d\n", n);
+  
+  // WRITE TO FILE HERE
+
+  for (int row = 0; row < boardSize; row++) {
+    for (int col = 0; col < boardSize; col++) {
+      int i = boardSize * row + col;
+      writeFile(output_file, board, i);
+    }
+    fprintf(output_file, "\n");
+  }
+  fclose(output_file);
+
+  return 0;
+}
